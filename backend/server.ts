212601fbd0b85ebdbd6d24ad0a2ae603f032329e
sershishkov <ser.shishkov@gmail.com>
2022-08-16 18:18:11 +0300

    start project

---
+++ backend/server.ts
@@ -0,0 +1,37 @@
+import path from 'path';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+import { errorHandler } from './middlewares/errorMiddleware';
+import connectDB from './config/db';
+dotenv.config();
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Запустились   ffff');
+});
+
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    );
+  });
+} else {
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Please set to production');
+  });
+}
+app.use(errorHandler);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
