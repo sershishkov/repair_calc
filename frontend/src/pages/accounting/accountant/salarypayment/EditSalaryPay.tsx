96336af4843ef4c7cdf362aa3cb99c5e19e3e333
sershishkov <ser.shishkov@gmail.com>
2022-11-11 16:06:57 +0200

    F paymenttosupplier

--- frontend/src/pages/accounting/accountant/salarypayment/EditSalaryPay.tsx
+++ frontend/src/pages/accounting/accountant/salarypayment/EditSalaryPay.tsx
@@ -1,7 +1,250 @@
-import React from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import dayjs, { Dayjs } from 'dayjs';
+
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
+import { RootState } from '../../../../app/store';
+
+import {
+  salarypayment__update,
+  salarypayment__get_one,
+} from '../../../../features/accounting/accountant/salarypayment/salarypayment__Slice';
+
+import { worker__get_all } from '../../../../features/accounting/refData/worker/worker__Slice';
+import { contract__get_all } from '../../../../features/accounting/refData/contract/contract__Slice';
+
+import {
+  I_Worker,
+  I_Contract,
+} from '../../../../interfaces/AccountingInterfaces';
+
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+import Stack from '@mui/material/Stack';
+
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+
+const initState = {
+  worker: '',
+  paymentSum: '',
+  contract: '',
+};
 
 const EditSalaryPay = () => {
-  return <div>EditSalaryPay</div>;
+  const { item, isLoading } = useAppSelector(
+    (state: RootState) => state.salarypayment__state
+  );
+
+  const arr__Workers = useAppSelector(
+    (state: RootState) => state.worker__state.items
+  );
+  const arr__Contracts = useAppSelector(
+    (state: RootState) => state.contract__state.items
+  );
+
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  let { id } = useParams();
+  const [formData, setFormdata] = useState(initState);
+  const [paymentDate, set_paymentDate] = React.useState<Dayjs | null>(
+    dayjs(new Date())
+  );
+
+  const { worker, paymentSum, contract } = formData;
+
+  useEffect(() => {
+    if (id) {
+      dispatch(salarypayment__get_one({ _id: id }));
+    }
+    dispatch(worker__get_all({ page: `0`, limit: `0` }));
+    dispatch(contract__get_all({ page: `0`, limit: `0` }));
+  }, [id, dispatch]);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormdata((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const created__Data = {
+      _id: id,
+      worker,
+      paymentSum: paymentSum ? Number(paymentSum) : 0,
+      contract,
+      navigate,
+    };
+
+    dispatch(salarypayment__update(created__Data));
+  };
+
+  useEffect(() => {
+    const inputFocus = document.getElementById('paymentSum');
+    inputFocus?.focus();
+  }, []);
+
+  useLayoutEffect(() => {
+    if (item) {
+      setFormdata({
+        paymentSum: item.paymentSum!.toString(),
+
+        worker:
+          typeof item.worker! === 'string' ? item.worker : item.worker?._id!,
+        contract:
+          typeof item.contract! === 'string'
+            ? item.contract
+            : item.contract?._id!,
+      });
+      set_paymentDate(item.paymentDate!);
+    }
+  }, [item]);
+
+  const handleChangeSelects = (event: SelectChangeEvent) => {
+    setFormdata((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value as string,
+    }));
+  };
+
+  const handleChangeDate = (newValue: Dayjs | null) => {
+    set_paymentDate(newValue);
+  };
+
+  const onClickAddItem = (link: string) => {
+    navigate(`/refdata/${link}/add`);
+  };
+
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
+  return (
+    <Grid
+      component='form'
+      onSubmit={onSubmit}
+      container
+      direction='column'
+      autoComplete='off'
+    >
+      <Grid item className='item item-heading'>
+        <Typography variant='h3' align='center'>
+          Редактировать
+        </Typography>
+      </Grid>
+      <Grid item>
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          name='paymentSum'
+          label='paymentSum'
+          type='number'
+          id='paymentSum'
+          value={paymentSum ?? ''}
+          onChange={onChange}
+        />
+      </Grid>
+
+      <Grid item sx={{ mb: 2 }}>
+        <DesktopDatePicker
+          label='Календарь'
+          inputFormat='DD-MM-YYYY'
+          value={paymentDate}
+          onChange={handleChangeDate}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </Grid>
+
+      <Grid item sx={{ mb: 2 }}>
+        <Stack
+          direction='row'
+          spacing={2}
+          // direction={{ xs: 'column', sm: 'row' }}
+        >
+          <FormControl fullWidth>
+            <InputLabel id='worker-label'>worker</InputLabel>
+            <Select
+              labelId='worker-label'
+              id='worker'
+              name='worker'
+              value={worker ?? ''}
+              label='worker'
+              onChange={handleChangeSelects}
+            >
+              {arr__Workers?.map((item: I_Worker) => (
+                <MenuItem key={item._id} value={item._id}>
+                  {`${item.lastName} ${item.firstName}`}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <IconButton onClick={() => onClickAddItem('worker')}>
+            <AddIcon color='success' sx={{ fontSize: 30 }} />
+          </IconButton>
+        </Stack>
+      </Grid>
+
+      <Grid item>
+        <Stack
+          direction='row'
+          spacing={2}
+          // direction={{ xs: 'column', sm: 'row' }}
+        >
+          <FormControl fullWidth>
+            <InputLabel id='contract-label'>contract</InputLabel>
+            <Select
+              labelId='contract-label'
+              id='contract'
+              name='contract'
+              value={contract ?? ''}
+              label='contract'
+              onChange={handleChangeSelects}
+            >
+              {arr__Contracts?.map((item: I_Contract) => (
+                <MenuItem key={item._id} value={item._id}>
+                  {`${
+                    typeof item.client !== 'string'
+                      ? item.client?.nameClientShort!
+                      : item.client
+                  }, ${item.contractDescription}`}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <IconButton onClick={() => onClickAddItem('contract')}>
+            <AddIcon color='success' sx={{ fontSize: 30 }} />
+          </IconButton>
+        </Stack>
+      </Grid>
+
+      <Grid item>
+        <Button
+          type='submit'
+          fullWidth
+          disabled={!worker || !paymentSum || !paymentDate || !contract}
+          variant='contained'
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Сохранить
+        </Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default EditSalaryPay;
--- frontend/src/pages/accounting/accountant/salarypayment/ListSalaryPay.tsx
+++ frontend/src/pages/accounting/accountant/salarypayment/ListSalaryPay.tsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import { useAppSelector } from '../../../../app/hooks';
+import { RootState } from '../../../../app/store';
+import dayjs from 'dayjs';
 
-const ListSalaryPay = () => {
-  return <div>ListSalaryPay</div>;
-};
+import MyIconButtonAdd from '../../../../components/account/MyIconButtonAdd';
+
+import TableSimple from '../../../../components/account/TableSimple';
+
+import {
+  salarypayment__get_all,
+  salarypayment__delete_one,
+} from '../../../../features/accounting/accountant/salarypayment/salarypayment__Slice';
+import { I_SalaryPayment } from '../../../../interfaces/AccountingInterfaces';
+
+const editLink = `/accounting/salarypayment`;
+
+interface NewObject {
+  _id: string;
+  worker: string;
+  paymentSum: string;
+  paymentDate: string;
+  clientName: string;
+}
+
+function ListSalaryPay() {
+  const { items, total, isLoading } = useAppSelector(
+    (state: RootState) => state.salarypayment__state
+  );
+  const itemsToDisplay = items?.map((item: I_SalaryPayment) => {
+    const newObject: NewObject = {
+      _id: item._id!,
+      worker:
+        typeof item.worker !== 'string'
+          ? `${item.worker?.lastName!} ${item.worker?.firstName!}`
+          : item.worker!,
+      paymentSum: item.paymentSum!.toString(),
+      paymentDate: dayjs(item.paymentDate!).format('DD-MM-YYYY'),
+
+      clientName:
+        typeof item.contract !== 'string'
+          ? typeof item.contract?.client !== 'string'
+            ? item.contract?.client?.nameClientShort!
+            : item.contract?.client
+          : item.contract!,
+    };
+
+    return newObject;
+  });
+
+  const headerFields = ['worker', 'paymentSum', 'paymentDate', 'clientName'];
+  const tableFields = ['worker', 'paymentSum', 'paymentDate', 'clientName'];
+  return (
+    <>
+      <MyIconButtonAdd href={`${editLink}/add`} />
+
+      <TableSimple
+        items={itemsToDisplay}
+        total={total}
+        isLoading={isLoading}
+        get__all={salarypayment__get_all}
+        delete__one={salarypayment__delete_one}
+        headerFields={headerFields}
+        tableFields={tableFields}
+        editLink={editLink}
+      />
+    </>
+  );
+}
 
 export default ListSalaryPay;
